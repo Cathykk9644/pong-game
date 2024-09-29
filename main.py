71b9edb12a71c6d6cecff67d51d9e4f767c36795
Cathy Hui <erashine414@icloud.com>
2024-09-29 12:18:15 +0800

    implement collision detection with top, bottom walls and paddles

--- main.py
+++ main.py
@@ -22,9 +22,20 @@ screen.onkey(left_paddle.move_down, "s")
 
 game_running = True
 while game_running:
-  time.sleep(0.05)
+  time.sleep(0.1)
   screen.update()
-  ball.move(dx=10, dy=10)
+  ball.move()
+
+  # Detect collision with wall
+  if ball.ycor() > 280 or ball.ycor() < -280:
+    ball.bounce()
+
+  # Detect collision with paddles
+  if ball.distance(right_paddle) < 50 and (ball.xcor() > 330 and ball.xcor() < 350):
+    ball.x_move *= -1
+
+  if ball.distance(left_paddle) < 50 and (ball.xcor() < -330 and ball.xcor() > -350):
+    ball.x_move *= -1
 
 screen.exitonclick()
 
