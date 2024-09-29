afef095ab2a421d077410747fb871ed9a4258f9a
Cathy Hui <erashine414@icloud.com>
2024-09-29 17:23:20 +0800

    implement scoreboard class for recording scores

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from score import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0) # turn off animations
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(right_paddle.move_up, "Up")
@@ -37,10 +39,12 @@ while game_running:
   # Detect Right Paddle misses
   if ball.xcor() > 380:
     ball.reset_position()
+    scoreboard.increase_left_score()
   
   # Detect left Paddle misses
   if ball.xcor() < -380:
     ball.reset_position()
+    scoreboard.increase_right_score()
   
 screen.exitonclick()
 
