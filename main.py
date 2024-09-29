ae87dab3b4ed90c025da5c8d5071065dd681b52a
Cathy Hui <erashine414@icloud.com>
2024-09-29 11:48:26 +0800

    implement paddle class for making paddles for play

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
+screen.tracer(0) # turn off animations
+
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(right_paddle.move_up, "Up")
+screen.onkey(right_paddle.move_down, "Down")
+
+screen.onkey(left_paddle.move_up, "w")
+screen.onkey(left_paddle.move_down, "s")
+
+game_running = True
+while game_running:
+  screen.update()
 
 screen.exitonclick()
+
